525a983700e5d9533d79dbbc5888541209eb0e53
jgerardsimcock <justin_simcock@yahoo.com>
2016-09-13 09:40:31 -0700

    add rtd config to setup.py and config.py

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['paramiko', 'getpass', 'time']
+MOCK_MODULES = ['paramiko']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------
