908e83bef401c9027d8e8fc81a771adf19a8601e
James Rising <jarising@gmail.com>
2018-05-17 14:43:13 +0100

    want to order prompts

--- computer/configure.py
+++ computer/configure.py
@@ -1,3 +1,4 @@
+import copy
 from local_server import LocalServer
 from login_server import LoginServer
 
@@ -7,44 +8,46 @@ class UserException(Exception):
     pass
 
 def config_or_prompt_several(mapping, config, prompter, callback):
-    config_or_prompt_remaining(copy.copy(mapping), mapping, copy.copy(config), config, prompter, callback)
+    return config_or_prompt_remaining(copy.copy(mapping), mapping, copy.copy(config), config, prompter, callback)
 
 def config_or_prompt_remaining(mapping, mapping_remaining, original_config, config, prompter, callback):
-    key = mapping.next()
-    if len(mapping) == 1:
+    key = mapping_remaining.keys()[0]
+    if len(mapping_remaining) == 1:
         def getnext(config):
             try:
-                callback(config)
-            except:
-                config_or_prompt_several(mapping, original_config, prompter, callback)
+                return callback(config)
+            except Exception as ex:
+                print ex
+                return config_or_prompt_several(mapping, original_config, prompter, callback)
     else:
-        newmapping = copy.copy(mapping)
+        newmapping = copy.copy(mapping_remaining)
         del newmapping[key]
         def getnext(config):
-            config_or_prompt_remaining(mapping, newmapping, original_config, config, prompter, callback)
+            return config_or_prompt_remaining(mapping, newmapping, original_config, config, prompter, callback)
             
     return config_or_prompt(key, mapping[key], config, prompter, getnext)
 
 def config_or_prompt(key, prompt, config, prompter, callback):
     """Get config value from either the config dict or a prompt; repeating as necessary until callback succeeds."""
-    try:
-        if key in config:
-            return callback(config)
-    except ex:
-        print ex
-        config[key] = prompter(prompt)
-        return config_or_prompt(key, prompt, config, prompter)
+    if key in config:
+        return callback(config)
 
-def initialize_server(config, prompter):
+    config[key] = prompter(prompt)
+    return callback(config)
+
+def initialize_server(config, prompter, callback):
     assert 'type' in config and 'utup' in config and 'cpus' in config and 'roots' in config, "Incomplete configuration: always define type, utup, cpus, and roots."
     assert config['type'] in server_types, "Unknown server type %s" % config['type']
 
     Server = server_types[config['type']]
 
     if Server == LocalServer:
-        return Server(config['utup'], config['cpus'], config['roots'])
+        return callback(LocalServer(config['utup'], config['cpus'], config['roots']))
 
     if Server == LoginServer:
-        config_or_prompt('username', "Username:", config, prompter)
-        username = prompter("username")
-        
+        return callback(config_or_prompt_several({'domain': "Domain: ",
+                                                  'username': "Username: ",
+                                                  'password': "Password: "}, config, prompter,
+                                                 lambda config: LoginServer(config['utup'], config['cpus'], config['roots'], config)))
+    
+    
--- computer/tools/single.py
+++ computer/tools/single.py
@@ -1,9 +1,17 @@
 import sys, yaml
 import configure
+from local_server import LocalServer
 
 with open(sys.argv[1], 'r') as fp:
     config = yaml.load(fp)
-server = configure.initialize_server(config, raw_input)
+
+def connect(server):
+    if not isinstance(server, LocalServer):
+        server.connect()
+
+    return server
+
+server = configure.initialize_server(config, raw_input, connect)
 
 command = ' '.join(sys.argv[2:])
 output, error = server.run_command(command)
